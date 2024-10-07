7a93369dfbd9ebf5ed75ecec808b9f630467eb58
ArchanaVenket <rajalakshmivenkatesan8@gmail.com>
2024-10-07 05:37:29 +0530

    about1

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var typed = new typed(".text", {
-  Strings: ["Frontend Developer", "Web Developer"],
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Web Developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 100,
   loop: true,
 });
+ 
